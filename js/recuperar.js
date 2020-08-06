f7e7c135dbaf65e78fdab17b979268ff043be3cf
Matias Santillan <matiassantillan@selinsoftware.com>
2020-08-06 20:20:33 -0300

    sdads

--- js/recuperar.js
+++ js/recuperar.js
@@ -1,26 +1,38 @@
-re
 (function ($) {
     $("#btnActualizar").click(function() {
-        var parametros = new URLSearchParams(window.location.search)
-        var key = parametros.get("key");
-
+        var key = getQueryVariable("key");
+        
         if (key !== undefined)
         {
-            var settings = {
-                "url": "https://api.qarfinder.com.ar/Usuarios/" + key + "/Clave?clave=" + contraseña,
-                "method": "POST",
-                "timeout": 0,
-                "headers": {
-                "accept": "*/*",
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36",
-                "Cookie": "__cfduid=d2f61c42bfc4db7eee1ea58135531830e1595650261"
-                },
-            };
-            
-            $.ajax(settings).done(function (response) {
-                console.log(response);
+            sha512($("#txtClave").val()).then(clave => {
+                console.log(key,clave);
+                var settings = {
+                    "url": "https://api.qarfinder.com.ar/Usuarios/" + key + "/Clave?clave=" + clave,
+                    "method": "POST",
+                    "timeout": 0
+                };
+                
+                $.ajax(settings).done(function (response) {
+                    console.log(response);
+                });
             });
-
         }
     });
 })(jQuery);
+
+function sha512(str) {
+    return crypto.subtle.digest("SHA-512", new TextEncoder("utf-8").encode(str)).then(buf => {
+      return Array.prototype.map.call(new Uint8Array(buf), x=>(('00'+x.toString(16)).slice(-2))).join('');
+    });
+}
+
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
