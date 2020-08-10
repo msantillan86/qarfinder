673044055c69eaab411fd7d0d8c0cb0dcf158d5e
Matias Santillan <matiassantillan@selinsoftware.com>
2020-08-10 00:28:16 -0300

    se le agrega el ojito

--- js/recuperar.js
+++ js/recuperar.js
@@ -25,6 +25,32 @@
             });
         }
     });
+
+    $("#show_hide_password a").on('click', function(event) {
+        event.preventDefault();
+        if($('#show_hide_password input').attr("type") == "text"){
+            $('#show_hide_password input').attr('type', 'password');
+            $('#show_hide_password i').addClass( "fa-eye-slash" );
+            $('#show_hide_password i').removeClass( "fa-eye" );
+        }else if($('#show_hide_password input').attr("type") == "password"){
+            $('#show_hide_password input').attr('type', 'text');
+            $('#show_hide_password i').removeClass( "fa-eye-slash" );
+            $('#show_hide_password i').addClass( "fa-eye" );
+        }
+    });
+
+    $("#show_hide_password_repetir a").on('click', function(event) {
+        event.preventDefault();
+        if($('#show_hide_password_repetir input').attr("type") == "text"){
+            $('#show_hide_password_repetir input').attr('type', 'password');
+            $('#show_hide_password_repetir i').addClass( "fa-eye-slash" );
+            $('#show_hide_password_repetir i').removeClass( "fa-eye" );
+        }else if($('#show_hide_password_repetir input').attr("type") == "password"){
+            $('#show_hide_password_repetir input').attr('type', 'text');
+            $('#show_hide_password_repetir i').removeClass( "fa-eye-slash" );
+            $('#show_hide_password_repetir i').addClass( "fa-eye" );
+        }
+    });
 })(jQuery);
 
 
